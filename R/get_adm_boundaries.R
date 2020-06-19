2ea52dd08fee5fc892cae1b7e773f216492aaf64
Baboyma Kagniniwa <baboyma@gmail.com>
2020-06-18 21:56:36 -0400

    Update to get_adm_boundaries - Simplified options

--- R/get_adm_boundaries.R
+++ R/get_adm_boundaries.R
@@ -3,7 +3,7 @@
 #' This function extracts / reads geospatial data
 #' for specified country and administractive boundary levels
 #'
-#' @param country_name ISO3 code
+#' @param country_code ISO3 code
 #' @param adm_level country administrative boundaries level
 #' @param local save a copy?
 #' @param geo_path where to save / read data from
@@ -12,21 +12,14 @@
 #' @example
 #' \dontrun{
 #' get_adm_boundaries("UGA")
-#' get_adm_boundaries("UGA", adm_level=2, local=FALSE)
+#' get_adm_boundaries("UGA", adm_level=2, geo_path="GIS")
 #' }
 #'
-get_adm_boundaries <- function(country_name,
+get_adm_boundaries <- function(country_code,
                                adm_level=0,
-                               local=TRUE,
-                               geo_path=NULL) {
+                               download=TRUE,
+                               geo_path="") {
 
-    # Read data form local dir first
-        downlaod <- local != TRUE
-
-    # geodata location
-        if (!is.null(geo_path) ) {
-            geo_path <- here::here()
-        }
 
     # Extract data from GADM
         geo_data <- raster::getData("GADM",
@@ -34,11 +27,6 @@ get_adm_boundaries <- function(country_name,
                                     level = adm_level,
                                     download = downlaod,
                                     path = geo_path)
-
-    # Force a download
-        if (is.null(geo_data)) {
-            geo_data <- get_adm_boundaries(country_name, adm_level, local = FALSE)
-        }
 
     # Convert data into sf format
         geo_data <- sf::st_as_sf(geo_data) %>%
