7f75155e17ab1cef77aa99671e1fa839d669693a
Baboyma Kagniniwa <baboyma@gmail.com>
2024-02-09 08:08:31 -0500

    ADD - get_grids for square grid - follows get_hexbins

--- R/create_hexbins.R
+++ R/create_hexbins.R
@@ -1,4 +1,4 @@
-#' Get an hexbins polygon feature class for country ABC
+#' @title Create hexagonal grids
 #'
 #' @param spdf input spatial data frame
 #' @param size size of each hex bin in meters, default set to 15K meters (15KM)
@@ -44,7 +44,63 @@ get_hexbins <- function(spdf, size = 15000) {
 
   spdf_hex <- spdf %>%
     sf::st_transform(crs = to_crs) %>%
-    sf::st_make_grid(what = "polygones", square = FALSE, cellsize = size) %>%
+    sf::st_make_grid(what = "polygons", square = FALSE, cellsize = size) %>%
+    sf::st_intersection(spdf) %>%
+    sf::st_make_valid() %>%
+    sf::st_as_sf() %>%
+    sf::st_transform(crs = from_crs)
+
+  return(spdf_hex)
+}
+
+
+#' @title Create square grids
+#'
+#' @param spdf input spatial data frame
+#' @param size size of each hex bin in meters, default set to 15K meters (15KM)
+#' @return country hex polygon as feature class
+#' @export
+#' @examples
+#' \dontrun{
+#'  library(gisr)
+#'
+#'  shp <- get_admin0(countries = "Nigeria")
+#'
+#'  get_grids(shp, 10000)
+#' }
+#'
+get_grids <- function(spdf, size = 15000) {
+
+  # CRS
+
+  from_crs <- sf::st_crs(spdf) # Source CRS
+  to_crs <- sf::st_crs(3857)   # Projected CRS
+
+  # Dissolve if boundaries
+
+  if ( nrow(spdf) == 0 )
+    usethis::ui_stop("Input spatial data is empty.")
+
+  # Merge all features into one
+  spdf <- spdf %>%
+    dplyr::mutate(
+      id = dplyr::row_number(),
+      area = sf::st_area(.)
+    ) %>%
+    dplyr::group_by(id) %>%
+    dplyr::summarise(area = sum(area))
+
+  # Make any corrections
+
+  spdf <- spdf %>%
+    sf::st_make_valid() %>%
+    sf::st_as_sf()
+
+  # Create hexbins
+
+  spdf_hex <- spdf %>%
+    sf::st_transform(crs = to_crs) %>%
+    sf::st_make_grid(what = "polygons", square = TRUE, cellsize = c(size, size)) %>%
     sf::st_intersection(spdf) %>%
     sf::st_make_valid() %>%
     sf::st_as_sf() %>%
